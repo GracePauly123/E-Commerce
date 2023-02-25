514be839770ceb62dd1e2d0fe5ee65c69166ac51
Grace Pauly <gracious.pauly@gmail.com>
2023-02-25 08:50:58 -0500

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">gRacesite</a>
+      </header>
+     <main>
+      lists
+     </main>
+    </div>
+  );
+}
+
+export default App;
